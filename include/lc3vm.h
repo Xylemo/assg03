fe3de635ba33e47ab94d1f475f1b3b08c603879f
Xylemo <charliemicbobby@gmail.com>
2026-04-25 04:15:16 +0000

    Task 6 - Implemented

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -83,6 +83,10 @@ void update_flags(uint16_t modified_register);
 void add(uint16_t i);
 void andlc(uint16_t i);
 void notlc(uint16_t i);
+void ld(uint16_t i);
+void ldi(uint16_t i);
+void ldr(uint16_t i);
+void lea(uint16_t i);
 
 void rti(uint16_t i);
 void res(uint16_t i);
